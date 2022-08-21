15fa182c018c7b5a197c5ad361ad43ad72637c54
Tzu-Chao Wang <john29917958@gmail.com>
2022-08-22 00:10:38 +0800

    Enhance style

--- app.js
+++ app.js
@@ -23,50 +23,60 @@ window.onload = function () {
             let toDo = document.createElement('div');
     
             toDo.classList.add('row');
-            toDo.classList.add('g-3');
-            toDo.classList.add('align-items-center');
-            toDo.classList.add('mb-2');
     
             return toDo;
         }
 
-        inputContainer.classList.add('col-auto');
-        input.style.display = 'inline-block';
-        input.classList.add('form-control');
+        inputContainer.classList.add('input-field');
+        inputContainer.classList.add('col');
+        inputContainer.classList.add('m7');
+        inputContainer.style.marginTop = 0;
+        inputContainer.style.marginBottom = 0;
+        input.type = 'text';
+        input.style.marginBottom = 0;
+        input.placeholder = 'What should be done...';
         input.value = text;
         input.setAttribute('readonly', true);
 
-        buttons.classList.add('col-auto');
+        buttons.classList.add('col');
+        buttons.classList.add('m5');
+        buttons.style.marginTop = '6px';
 
         let icon = document.createElement('i');
-        icon.classList.add('fa-solid');
-        icon.classList.add('fa-pencil');
+        icon.classList.add('material-icons');
+        icon.classList.add('left');
+        icon.appendChild(document.createTextNode('create'));
         actionBtn.append(icon);
         let actionText = document.createTextNode(' Edit');
         actionBtn.append(actionText);
+        actionBtn.style.marginRight = "5px";
+        actionBtn.classList.add('waves-effect');
+        actionBtn.classList.add('waves-light');
         actionBtn.classList.add('btn');
-        actionBtn.classList.add('btn-primary');
-        actionBtn.classList.add('me-2');
+        actionBtn.classList.add('blue');
 
         icon = document.createElement('i');
-        icon.classList.add('fa-solid');
-        icon.classList.add('fa-xmark');
+        icon.classList.add('material-icons');
+        icon.classList.add('left');
+        icon.appendChild(document.createTextNode('clear'));
         cancelBtn.append(icon);
         let cancelText = document.createTextNode(' Cancel');
         cancelBtn.append(cancelText);
         cancelBtn.disabled = true;
-        cancelBtn.classList.add('btn');
-        cancelBtn.classList.add('btn-light');
-        cancelBtn.classList.add('me-2');
+        cancelBtn.style.marginRight = "5px";
+        cancelBtn.classList.add('waves-effect');
+        cancelBtn.classList.add('btn-flat');
 
         icon = document.createElement('i');
-        icon.classList.add('fa-solid');
-        icon.classList.add('fa-trash');
+        icon.classList.add('material-icons');
+        icon.classList.add('left');
+        icon.appendChild(document.createTextNode('delete'));
         deleteBtn.append(icon);
         let deleteText = document.createTextNode(' Delete');
         deleteBtn.append(deleteText);
-        deleteBtn.classList.add('btn');
-        deleteBtn.classList.add('btn-light');
+        deleteBtn.classList.add('waves-effect');
+        deleteBtn.classList.add('waves-red');
+        deleteBtn.classList.add('btn-flat');
 
         inputContainer.appendChild(input);
         buttons.appendChild(actionBtn);
