835ec9b95ef2e7b30805f39395b83a8885e09d45
Tzu-Chao Wang <john29917958@gmail.com>
2022-08-23 22:18:18 +0800

    Implement editing to-do task title feature

--- react/to-do/src/ToDo.js
+++ react/to-do/src/ToDo.js
@@ -10,19 +10,62 @@ class ToDo extends React.Component {
         this.#id = date.getFullYear().toString() + date.getMonth().toString() + date.getDate().toString() +
             date.getHours().toString() + date.getMinutes().toString() + date.getSeconds().toString() +
             date.getMilliseconds().toString();
-        
+
         this.state = {
             text: this.props.text,
-            isEditing: true
+            cachedText: null,
+            isEditing: false
         };
     }
 
+    #beginEdit() {
+        this.setState({
+            isEditing: true,
+            cachedText: this.state.text
+        });
+    }
+
+    #onInputValChanged(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    #applyChange(text) {
+        this.setState({
+            isEditing: false,
+            cachedText: null
+        });
+    }
+
     render() {
+        let action = this.state.isEditing ? this.#applyChange.bind(this) : this.#beginEdit.bind(this);
+
         return (
             <div className="row">
                 <div className="input-field col m5 s12">
-                    <input id={this.#id} type={"text"} value={this.state.text} disabled={!this.state.isEditing}></input>
+                    <input id={this.#id} type={"text"} value={this.state.text} disabled={!this.state.isEditing} readOnly={!this.state.isEditing} onChange={this.#onInputValChanged.bind(this)}></input>
                     <label htmlFor={this.#id}>Task Name</label>
+                </div>
+                <div className="col m7 s12" style={{ marginTop: "24px" }}>
+                    <button className="btn waves-effect waves-light blue" style={{ marginRight: "5px" }} onClick={action}>
+                        <i className="material-icons left">
+                            {this.state.isEditing ? "check" : "create"}
+                        </i>
+                        {this.state.isEditing ? "Apply" : "Edit"}
+                    </button>
+                    <button className="btn-flat waves-effect">
+                        <i className="material-icons left">
+                            clear
+                        </i>
+                        Cancel
+                    </button>
+                    <button className="btn-flat waves-effect waves-red">
+                        <i className="material-icons left">
+                            delete
+                        </i>
+                        Delete
+                    </button>
                 </div>
             </div>
         );
