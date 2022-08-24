6db93280c4c584377ec86bf52cfcab41dd4e175b
Tzu-Chao Wang <john29917958@gmail.com>
2022-08-25 07:44:59 +0800

    Fix to-do layout to make del btn stay in viewport

--- react/to-do/src/ToDo.js
+++ react/to-do/src/ToDo.js
@@ -61,7 +61,7 @@ class ToDo extends React.Component {
                         </label>
                     </p>
                 </div>
-                <div className="col s10">
+                <div className="col s9">
                     <input
                         id={this.props.id}
                         type={"text"}
@@ -74,7 +74,7 @@ class ToDo extends React.Component {
                         onBlur={this.#applyChange.bind(this)}
                         onKeyDown={this.#handleKeyDown.bind(this)} />
                 </div>
-                <div className="col s1">
+                <div className="col s2">
                     <button className="btn-flat waves-effect"
                         onClick={function () { self.props.onDelete(self.props.id); }}>
                         <i className="material-icons">
